489154f0ffade5708c5fbe6ff5320d4c2b699247
Vitalis Wiens <vitalis.wiens@gmail.com>
2020-08-14 10:19:56 +0200

    added temporary new components to handle the new statementstore

---
+++ src/components/ViewPaper/GraphView/GizmoGraph2.js
@@ -0,0 +1,279 @@
+import React, { Component } from 'react';
+import * as PropTypes from 'prop-types';
+import { getReseachProblemsOfContribution, loadCachedVersion } from 'actions/statementBrowser';
+import { selectContribution, updateResearchProblems } from 'actions/viewPaper';
+import { updateStatementStore } from 'actions/statementBrowserStoreActions';
+import { connect } from 'react-redux';
+import uniqBy from 'lodash/uniqBy';
+import { PREDICATES } from 'constants/graphSettings';
+
+class GizMOGraph extends Component {
+    constructor(props) {
+        super(props);
+        this.graphRoot = undefined;
+        this.graphVis = props.graphVis;
+
+        // parent functions called by child
+        this.updateDepthRange = props.updateDepthRange;
+    }
+
+    componentDidMount() {
+        console.log('The graph visualization is mounted!!! ');
+        const graph = this.createGraphDataFromStatementStore();
+        // if (!this.graphVis.graphInitialized()) {
+        this.graphVis.bindComponentValues({
+            graph: graph,
+            layout: this.props.layout,
+            graphBgColor: '#ecf0f1',
+            depth: this.props.depth
+        });
+    }
+
+    componentDidUpdate = prevProps => {
+        console.log('GizMOGraph Container Updates');
+        console.log(this.props);
+        this.createGraphDataFromStatementStore();
+
+        if (this.props.layout !== prevProps.layout) {
+            this.graphVis.updateLayout(this.props.layout);
+        }
+    };
+
+    componentWillUnmount() {
+        this.graphVis.stopBackgroundProcesses();
+        if (this.graphVis.graphIsInitialized) {
+            // todo : make sure memory is cleared!
+            this.clearGraphData();
+        }
+    }
+
+    createGraphDataFromStatementStore = () => {
+        console.log('Creating the new graph structure from the store');
+        console.log(this.props.contributionStatementStore);
+        const allNodes = [];
+        const allLinks = [];
+
+        // first fetch metadata store;
+        const mds = this.props.metaInformationStore ? this.props.metaInformationStore.statements : [];
+        const metaInformationNodesAndLinks = this.processStatements(mds, true);
+        console.log(metaInformationNodesAndLinks);
+        allNodes.push(...metaInformationNodesAndLinks.nodes);
+        allLinks.push(...metaInformationNodesAndLinks.edges);
+        // then fetch orcid author ids;
+        const authors_ods = this.props.authorsOrcidStore ? this.props.authorsOrcidStore.statements : [];
+        const authrosOrcidStatements = this.processStatements(authors_ods, false);
+        console.log(authrosOrcidStatements);
+        allNodes.push(...authrosOrcidStatements.nodes);
+        allLinks.push(...authrosOrcidStatements.edges);
+
+        // now extract the graph structure from the statement store;
+        const contribStore = this.props.contributionStatementStore;
+        console.log(contribStore, '<<');
+
+        const allContributionStatements = [];
+
+        for (const name in contribStore) {
+            console.log('Extracting graph information for contribution:', name);
+            const subjects = contribStore[name].resources;
+            const predicates = contribStore[name].properties;
+            const objects = contribStore[name].values;
+
+            const currentContributionStatements = [];
+            subjects.allIds.forEach(id => {
+                const resource = subjects.byId[id];
+                resource.id = resource.existingResourceId;
+
+                if (resource.propertyIds.length > 0) {
+                    // try to create a statement;
+                    resource.propertyIds.forEach(propId => {
+                        const property = predicates.byId[propId];
+                        property.id = property.existingPredicateId;
+
+                        if (property.valueIds.length > 0) {
+                            property.valueIds.forEach(valId => {
+                                const object = objects.byId[valId];
+                                object.id = object.resourceId;
+                                if (object.type === 'object') {
+                                    object._class = 'resource';
+                                }
+                                console.log({ subject: resource, predicate: property, object: object });
+                                currentContributionStatements.push({ subject: resource, predicate: property, object: object });
+                            });
+                        }
+                    });
+                }
+            });
+            allContributionStatements.push(currentContributionStatements);
+        }
+        // try to process the individual contribution arrays as graphs;
+        allContributionStatements.forEach(subArray => {
+            const subGraph = this.processStatements(subArray, false);
+            console.log(subGraph);
+            allNodes.push(...subGraph.nodes);
+            allLinks.push(...subGraph.edges);
+        });
+
+        // remove duplicate nodes
+        const graphNodes = uniqBy(allNodes, 'id');
+        const graphLinks = uniqBy(allLinks, e => [e.from, e.to, e.label].join());
+
+        console.log(graphNodes);
+        console.log(graphLinks);
+
+        return { nodes: graphNodes, edges: graphLinks };
+    };
+
+    processSingleStatement = (nodes, edges, statement) => {
+        const subjectLabel = statement.subject.label.substring(0, 20);
+        const objectLabel = statement.object.label.substring(0, 20);
+
+        nodes.push({
+            id: statement.subject.id,
+            label: subjectLabel,
+            title: statement.subject.label,
+            classificationArray: statement.subject.classes
+        });
+
+        // check if node type is resource or literal
+        if (statement.object._class === 'resource') {
+            nodes.push({
+                id: statement.object.id,
+                label: objectLabel,
+                title: statement.object.label,
+                classificationArray: statement.object.classes,
+                isResearchFieldRelated:
+                    statement.predicate.id === PREDICATES.HAS_RESEARCH_FIELD || statement.predicate.id === PREDICATES.HAS_SUB_RESEARCH_FIELD
+            });
+        } else {
+            nodes.push({
+                id: statement.object.id,
+                label: objectLabel,
+                title: statement.object.label,
+                type: 'literal'
+            });
+        }
+
+        if (statement.predicate.id === PREDICATES.HAS_AUTHOR) {
+            // add user Icon to target node if we have 'has author' property === P27
+            edges.push({
+                from: statement.subject.id,
+                to: statement.object.id,
+                label: statement.predicate.label,
+                isAuthorProp: true,
+                predicateId: statement.predicate.id
+            });
+        } else if (statement.predicate.id === PREDICATES.HAS_DOI) {
+            // add DOI Icon to target node
+            edges.push({
+                from: statement.subject.id,
+                to: statement.object.id,
+                label: statement.predicate.label,
+                isDOIProp: false,
+                predicateId: statement.predicate.id
+            }); // remove doi icon for now
+        } else {
+            // no Icon for the target node
+            edges.push({
+                from: statement.subject.id,
+                to: statement.object.id,
+                label: statement.predicate.label,
+                predicateId: statement.predicate.id
+            });
+        }
+    };
+
+    processStatements = (statements, auxNode) => {
+        let nodes = [];
+        let edges = [];
+
+        for (const statement of statements) {
+            this.processSingleStatement(nodes, edges, statement);
+        }
+        // remove duplicate nodes
+        nodes = uniqBy(nodes, 'id');
+        edges = uniqBy(edges, e => [e.from, e.to, e.label].join());
+
+        if (statements.length > 0 && auxNode) {
+            // find node with paper resource ID;
+            // heuristic its always node 0;
+            // >> auxNode will only be set in the loadStatements function (only depth 0 statements)
+            // which is only called on onOpened()
+
+            const meta = {
+                id: '__META_NODE__',
+                label: 'Meta Information',
+                title: 'Meta Information',
+                classificationArray: []
+            };
+            const link = {
+                from: nodes[0].id,
+                to: meta.id,
+                label: 'has meta information'
+            };
+
+            nodes.push(meta);
+            edges.push(link);
+
+            edges.forEach(edge => {
+                if (edge.predicateId) {
+                    if (
+                        edge.predicateId === PREDICATES.HAS_DOI ||
+                        edge.predicateId === PREDICATES.HAS_VENUE ||
+                        edge.predicateId === PREDICATES.HAS_AUTHOR ||
+                        edge.predicateId === PREDICATES.HAS_PUBLICATION_MONTH ||
+                        edge.predicateId === PREDICATES.HAS_PUBLICATION_YEAR ||
+                        edge.predicateId === PREDICATES.HAS_RESEARCH_FIELD ||
+                        edge.predicateId === PREDICATES.HAS_SUB_RESEARCH_FIELD ||
+                        edge.predicateId === PREDICATES.URL
+                    ) {
+                        edge.from = meta.id;
+                    }
+                }
+            });
+        }
+
+        return { nodes: nodes, edges: edges };
+    };
+
+    centerGraphEvent() {
+        this.graphVis.zoomToExtent();
+    }
+
+    clearGraphData() {
+        this.graphVis.svgRoot.remove();
+    }
+
+    /** Component Rendering Function **/
+    render() {
+        return <div id="graphRendering" style={{ width: '100%', height: '100%', backgroundColor: 'gray' }} />;
+    }
+}
+
+/** Property Type Validation **/
+GizMOGraph.propTypes = {
+    updateDepthRange: PropTypes.func.isRequired,
+    graphVis: PropTypes.any.isRequired,
+    depth: PropTypes.any.isRequired,
+    layout: PropTypes.any.isRequired,
+
+    metaInformationStore: PropTypes.object.isRequired,
+    authorsOrcidStore: PropTypes.object.isRequired,
+    contributionStatementStore: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => {
+    return {
+        statementBrowserStore: state.statementBrowserStore,
+        contributionStatementStore: state.statementBrowserStore.contributionStatementStore,
+        metaInformationStore: state.statementBrowserStore.metaInformationStore,
+        authorsOrcidStore: state.statementBrowserStore.authorsOrcidStore
+    };
+};
+
+// TODO : add graph interactions (connect to store actions)
+const mapDispatchToProps = dispatch => ({});
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(GizMOGraph);
---
+++ src/components/ViewPaper/GraphView/GizmoGraphViewModal2.js
@@ -0,0 +1,252 @@
+import React, { Component } from 'react';
+import * as PropTypes from 'prop-types';
+import { Modal, ModalHeader, ModalBody, Input, Form, FormGroup, Label, Button } from 'reactstrap';
+
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
+import { faSpinner, faProjectDiagram, faHome, faSitemap, faExpandArrowsAlt } from '@fortawesome/free-solid-svg-icons';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+
+// moving GraphVis here in order to maintain the layouts and status related stuff;
+import GizmoGraph from './GizmoGraph2';
+import GraphVis from '../../../libs/gizmo/GraphVis';
+import SearchAutoComplete from './SearchAutoComplete';
+
+class GraphView extends Component {
+    constructor(props) {
+        super(props);
+        this.searchComponent = React.createRef();
+        this.seenDepth = -1;
+        this.graphVis = new GraphVis();
+        this.graphVis.propagateMaxDepthValue = this.updateDepthRange;
+        this.graphVis.propagateDictionary = this.propagateDictionary;
+    }
+
+    state = {
+        initializeGraph: false,
+        seenDepth: -1,
+        depth: 1,
+        maxDepth: 25,
+        seenFullGraph: false,
+        layoutSelectionOpen: false,
+        exploringFullGraph: false,
+        layout: 'force',
+        windowHeight: 0 // want this for auto aligning the size of the modal window
+    };
+
+    componentDidMount() {
+        window.addEventListener('resize', this.updateDimensions);
+        this.updateDimensions();
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateDimensions);
+    }
+
+    propagateDictionary = () => {
+        this.searchComponent.current.updateDictionary();
+    };
+
+    centerGraph = () => {
+        this.graphVis.zoomToExtent();
+    };
+
+    // not used anymore, stays for mem leak analysis (todo)
+    clearGraphData = () => {
+        this.child.current.clearGraphData();
+    };
+
+    updateDimensions = () => {
+        // test
+        const offset = 28 * 2 + 65;
+        this.setState({ windowHeight: window.innerHeight - offset });
+    };
+
+    handleDepthChange = event => {
+        if (event.target.value) {
+            this.setState({ depth: parseInt(event.target.value) });
+            this.graphVis.depthUpdateEvent(parseInt(event.target.value)).then();
+        }
+    };
+
+    exploreTheFullGraph = async () => {
+        this.setState({ exploringFullGraph: true });
+        await this.graphVis.fullExplore();
+        this.setState({ exploringFullGraph: false });
+    };
+
+    updateDepthRange = (value, fullGraph) => {
+        // called from the child to ensure that the depth has correct range
+        if (fullGraph) {
+            this.setState({ maxDepth: value, depth: value, seenFullGraph: true });
+            return;
+        }
+        if (value < this.state.depth || this.state.seenFullGraph === true) {
+            // Case we have seen the full Graph so we just update the graph view
+            this.setState({ depth: value });
+        } else {
+            if (!fullGraph && this.state.seenFullGraph === false) {
+                //Case we want to load more data
+                this.setState({ depth: value, seenFullGraph: false });
+            } else {
+                //Case we have seen the full Graph on going deeper
+                this.setState({ maxDepth: value, seenFullGraph: true });
+            }
+        }
+    };
+
+    renderHeader = () => {
+        return (
+            <div className="d-flex" style={{ height: '40px' }}>
+                <div style={{ width: '300px', height: '40px', paddingTop: '5px' }}>Paper graph visualization</div>
+                <div style={{ width: '100%', display: 'flex' }}>
+                    <Form inline className="ml-4">
+                        <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                            <Label for="depth" className="mb-0 mr-2">
+                                Depth
+                            </Label>
+                            <Input
+                                type="number"
+                                name="select"
+                                id="depth"
+                                bsSize="sm"
+                                onChange={this.handleDepthChange}
+                                onKeyDown={event => {
+                                    // prevent the reload when enter is pressed
+                                    if (event.keyCode === 13) {
+                                        event.preventDefault();
+                                        this.graphVis.depthUpdateEvent(this.state.depth).then();
+                                    }
+                                }}
+                                value={this.state.depth}
+                                style={{ width: '60px' }}
+                                min="1"
+                                max={this.state.maxDepth}
+                            />
+                        </FormGroup>
+                    </Form>
+                    <div style={{ flexDirection: 'row', display: 'flex', flexGrow: '1' }}>
+                        <Button
+                            color="darkblue"
+                            size="sm"
+                            style={{ margin: '0 10px', flexGrow: '1', display: 'flex', alignSelf: 'center', width: '155px' }}
+                            onClick={this.exploreTheFullGraph}
+                            disabled={this.state.exploringFullGraph}
+                        >
+                            {!this.state.exploringFullGraph ? (
+                                <>
+                                    <Icon icon={faExpandArrowsAlt} className="mr-1 align-self-center" />
+                                    Expand all nodes{' '}
+                                </>
+                            ) : (
+                                <>
+                                    <Icon icon={faSpinner} spin className="mr-1 align-self-center" /> Expanding graph
+                                </>
+                            )}
+                        </Button>
+                        <Button
+                            color="darkblue"
+                            size="sm"
+                            style={{ margin: '0 10px', flexGrow: '1', display: 'flex', alignSelf: 'center', width: '130px' }}
+                            onClick={this.centerGraph}
+                        >
+                            <Icon icon={faHome} className="mr-1 align-self-center" /> Center graph
+                        </Button>
+                        <Dropdown
+                            color="darkblue"
+                            size="sm"
+                            style={{ marginLeft: '10px', flexGrow: '1', display: 'flex', height: 'min-content', paddingTop: '5px' }}
+                            isOpen={this.state.layoutSelectionOpen}
+                            toggle={() => {
+                                this.setState({ layoutSelectionOpen: !this.state.layoutSelectionOpen });
+                            }}
+                        >
+                            <DropdownToggle caret color="darkblue">
+                                Layout:
+                                <Icon
+                                    icon={this.state.layout === 'force' ? faProjectDiagram : faSitemap}
+                                    rotation={this.state.layout === 'treeH' ? 270 : undefined}
+                                    className="mr-1"
+                                    style={{ width: '40px' }}
+                                />
+                            </DropdownToggle>
+                            <DropdownMenu>
+                                <DropdownItem
+                                    onClick={() => {
+                                        if (this.state.layout === 'force') {
+                                            return;
+                                        }
+                                        this.setState({ layout: 'force' });
+                                    }}
+                                >
+                                    <Icon icon={faProjectDiagram} className="mr-1" style={{ width: '40px' }} />
+                                    Force directed
+                                </DropdownItem>
+                                <DropdownItem
+                                    onClick={() => {
+                                        if (this.state.layout === 'treeH') {
+                                            // forcing reset of the layout
+                                            this.graphVis.updateLayout('treeH');
+                                            return;
+                                        }
+                                        this.setState({ layout: 'treeH' });
+                                    }}
+                                >
+                                    <Icon icon={faSitemap} rotation={270} className="mr-1" style={{ width: '40px' }} />
+                                    Horizontal tree
+                                </DropdownItem>
+                                <DropdownItem
+                                    onClick={() => {
+                                        if (this.state.layout === 'treeV') {
+                                            // forcing reset of the layout
+                                            this.graphVis.updateLayout('treeV');
+                                            return;
+                                        }
+                                        this.setState({ layout: 'treeV' });
+                                    }}
+                                >
+                                    <Icon icon={faSitemap} className="mr-1" style={{ width: '40px' }} />
+                                    Vertical tree
+                                </DropdownItem>
+                            </DropdownMenu>
+                        </Dropdown>
+                        <SearchAutoComplete ref={this.searchComponent} placeHolder="Search" graphVis={this.graphVis} />
+                    </div>
+                </div>
+            </div>
+        );
+    };
+
+    render() {
+        return (
+            <Modal
+                isOpen={this.props.showDialog}
+                toggle={this.props.toggle}
+                size="lg"
+                onOpened={() => {
+                    //  TODO
+                }}
+                style={{ maxWidth: '90%', marginBottom: 0 }}
+            >
+                <ModalHeader toggle={this.props.toggle}>{this.renderHeader()}</ModalHeader>
+                <ModalBody style={{ padding: '0', minHeight: '100px', height: this.state.windowHeight }}>
+                    <GizmoGraph
+                        depth={this.state.depth}
+                        updateDepthRange={this.updateDepthRange}
+                        initializeGraph={this.state.initializeGraph}
+                        maxDepth={this.state.maxDepth}
+                        layout={this.state.layout}
+                        graphVis={this.graphVis}
+                        graphBgColor="#ecf0f1"
+                    />
+                </ModalBody>
+            </Modal>
+        );
+    }
+}
+
+GraphView.propTypes = {
+    showDialog: PropTypes.bool.isRequired,
+    toggle: PropTypes.func.isRequired
+};
+
+export default GraphView;
