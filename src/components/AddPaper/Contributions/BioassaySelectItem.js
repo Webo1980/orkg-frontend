503aa0e3ab10ea6a944fde66bbe67fa0684443ea
Kheir Eddine Farfar <kfarfar@gmail.com>
2021-04-29 17:20:30 +0200

    highlight the text

--- src/components/AddPaper/Contributions/BioassaySelectItem.js
+++ src/components/AddPaper/Contributions/BioassaySelectItem.js
@@ -1,4 +1,4 @@
-import { Label, ListGroup } from 'reactstrap';
+import { Label, ListGroup, Alert } from 'reactstrap';
 import PropTypes from 'prop-types';
 import { StatementsGroupStyle, PropertyStyle, ValuesStyle, ValueItemStyle } from 'components/StatementBrowser/styled';
 import { reverse } from 'named-urls';
@@ -7,52 +7,56 @@ import ROUTES from 'constants/routes.js';
 
 export default function BioassaySelectItem(props) {
     return (
-        <ListGroup className="listGroupEnlarge">
-            <div className="mb-3">Please select correct values that you want to include in the contribution data:</div>
-            <div>
-                {Object.keys(props.data.labels).map(labelKey => (
-                    <StatementsGroupStyle key={`p${props.data.properties[labelKey]}`} className="noTemplate">
-                        <div className="row no-gutters">
-                            <PropertyStyle className="col-4" tabIndex="0">
-                                <div className="propertyLabel">
-                                    <Label className=" mr-2">
-                                        <Link
-                                            className="text-dark"
-                                            target="_blank"
-                                            to={reverse(ROUTES.PROPERTY, { id: props.data.properties[labelKey] })}
-                                        >
-                                            {labelKey}
-                                        </Link>
-                                    </Label>
-                                </div>
-                            </PropertyStyle>
-                            <ValuesStyle className="col-8 valuesList">
-                                <ListGroup flush className="px-3">
-                                    {props.data.labels[labelKey].map(value => (
-                                        <ValueItemStyle key={`p${props.data.resources[value]}`} className="suggested_value d-flex">
-                                            <div className="flex-grow-1">
-                                                <Label>
-                                                    <Link target="_blank" to={reverse(ROUTES.RESOURCE, { id: props.data.resources[value] })}>
-                                                        {value}
-                                                    </Link>
-                                                </Label>
-                                            </div>
-                                            <div>
-                                                <input
-                                                    type="checkbox"
-                                                    onChange={e => props.handleSelect(labelKey, value)}
-                                                    checked={!!props.selectedItems?.[labelKey]?.includes(value)}
-                                                />
-                                            </div>
-                                        </ValueItemStyle>
-                                    ))}
-                                </ListGroup>
-                            </ValuesStyle>
-                        </div>
-                    </StatementsGroupStyle>
-                ))}
+        <>
+            <div className="mb-3">
+                <Alert>Please select specific items that you want to include in the contribution data</Alert>
             </div>
-        </ListGroup>
+            <ListGroup className="listGroupEnlarge">
+                <div>
+                    {Object.keys(props.data.labels).map(labelKey => (
+                        <StatementsGroupStyle key={`p${props.data.properties[labelKey]}`} className="noTemplate">
+                            <div className="row no-gutters">
+                                <PropertyStyle className="col-4" tabIndex="0">
+                                    <div className="propertyLabel">
+                                        <Label className=" mr-2">
+                                            <Link
+                                                className="text-dark"
+                                                target="_blank"
+                                                to={reverse(ROUTES.PROPERTY, { id: props.data.properties[labelKey] })}
+                                            >
+                                                {labelKey}
+                                            </Link>
+                                        </Label>
+                                    </div>
+                                </PropertyStyle>
+                                <ValuesStyle className="col-8 valuesList">
+                                    <ListGroup flush className="px-3">
+                                        {props.data.labels[labelKey].map(value => (
+                                            <ValueItemStyle key={`p${props.data.resources[value]}`} className="suggested_value d-flex">
+                                                <div className="flex-grow-1">
+                                                    <Label>
+                                                        <Link target="_blank" to={reverse(ROUTES.RESOURCE, { id: props.data.resources[value] })}>
+                                                            {value}
+                                                        </Link>
+                                                    </Label>
+                                                </div>
+                                                <div>
+                                                    <input
+                                                        type="checkbox"
+                                                        onChange={e => props.handleSelect(labelKey, value)}
+                                                        checked={!!props.selectedItems?.[labelKey]?.includes(value)}
+                                                    />
+                                                </div>
+                                            </ValueItemStyle>
+                                        ))}
+                                    </ListGroup>
+                                </ValuesStyle>
+                            </div>
+                        </StatementsGroupStyle>
+                    ))}
+                </div>
+            </ListGroup>
+        </>
     );
 }
 
